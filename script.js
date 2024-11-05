e7cf2770ebcdf0580c7fcfc5e565af48692a1af4
Ayimuhamad74 <ayi10222@sttcipasung.ac.id>
2024-11-05 22:24:58 +0700

    menambahkan page ti dan styling

--- script.js
+++ script.js
@@ -1,29 +1,5 @@
 console.log('Hello!');
-// animasi ketika di scroll di page informatika
 
-  document.addEventListener("DOMContentLoaded", function() {
-    const elements = document.querySelectorAll('.informatika .row .content h3, .informatika .row .content p');
-
-    const options = {
-      root: null,
-      rootMargin: '0px',
-      threshold: 0.1
-    };
-
-    const observer = new IntersectionObserver((entries, observer) => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          entry.target.classList.add('visible');
-          observer.unobserve(entry.target); // Menghentikan pengamatan pada elemen setelah terlihat
-        }
-      });
-    }, options);
-
-    elements.forEach(element => {
-      element.classList.add('fade-in');
-      observer.observe(element);
-    });
-  });
   const header = document.querySelector(".calendar h1");
   const dates = document.querySelector(".dates");
   const navs = document.querySelectorAll("#prev, #next");
---
+++ src/if.js
@@ -0,0 +1,25 @@
+// animasi ketika di scroll di page informatika
+
+document.addEventListener("DOMContentLoaded", function() {
+    const elements = document.querySelectorAll('.informatika .row .content h3, .informatika .row .content p');
+
+    const options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0.1
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('visible');
+          observer.unobserve(entry.target); // Menghentikan pengamatan pada elemen setelah terlihat
+        }
+      });
+    }, options);
+
+    elements.forEach(element => {
+      element.classList.add('fade-in');
+      observer.observe(element);
+    });
+  });
